42d30f404c2821a2c179ae8906e8ebb328f1932c
viktor0110 <viktor-stefanov@mail.bg>
2023-01-28 17:00:40 +0200

    update p08

--- SoftUniFundamentals/arraysLab/P08.condenseArrayToNumber.js
+++ SoftUniFundamentals/arraysLab/P08.condenseArrayToNumber.js
@@ -1,4 +1,15 @@
-function condenseArrayToNumber() {
-
+function condenseArrayToNumber(arr) {
+   let currArr = arr;
+   while (currArr.length > 1) {
+    let newArr = [];
+    for(let i = 0; i < currArr.length-1; i++) {
+         let firstElement= currArr[i];
+         let secondElement = currArr[i+1];
+        
+         newArr.push(firstElement + secondElement);
+    }
+    currArr = newArr;
+}
+console.log(currArr[0])
 }
 condenseArrayToNumber([2,10,3])
