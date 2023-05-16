8fb18153ea99178ed0296f68b0f68b02690543c0
viktor0110 <viktor-stefanov@mail.bg>
2023-05-16 20:34:53 +0300

    uploading last done tasks

--- SoftUniJSAdvanced/DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/app.js
+++ SoftUniJSAdvanced/DOM Manipulation and Events - Exercise/05. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,26 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    let firstTextAreaElement = document.querySelector('#main div:nth-of-type(1) textarea');
+    let resultTextAreaElement = document.querySelector('#main div:nth-of-type(2) textarea');
+    let encodeButtonElement = document.querySelector('#main div:nth-of-type(1) button ');
+    let decodeButtonElement = document.querySelector('#main div:nth-of-type(2) button ');
+    let textForEncode;
+    let decodedText = [];
+    let result = [];
+
+    encodeButtonElement.addEventListener('click', () => {
+         textForEncode = firstTextAreaElement.value;
+        firstTextAreaElement.value = "";
+        Array.from(textForEncode).forEach((charr,i) => {
+            let currCharr = textForEncode.charCodeAt(i);
+            decodedText.push(currCharr + 1);
+            resultTextAreaElement.value = decodedText.join('');
+        })
+    })
+    decodeButtonElement.addEventListener('click', () => {
+        decodedText.forEach((charr, i) => {
+             result.push(String.fromCharCode(charr-1))
+        })
+
+        resultTextAreaElement.value = result.join('');
+    })
 }
