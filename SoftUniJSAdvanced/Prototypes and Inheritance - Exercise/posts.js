a95e4029455b2d8a3b5c60a765aa70c0ed336198
viktor0110 <viktor-stefanov@mail.bg>
2023-05-29 20:35:52 +0300

    upload tasks from prototypes and inheritance

---
+++ SoftUniJSAdvanced/Prototypes and Inheritance - Exercise/posts.js
@@ -0,0 +1,14 @@
+const object1 = {
+  property1: 42
+};
+
+// Object.seal(object1);
+object1.property1 = 33;
+console.log(object1.property1);
+// Expected output: 33
+console.log(object1.property1);
+object1['name'] = 42;
+
+// delete object1.property1; // Cannot delete when sealed
+console.log(object1);
+// Expected output: 33
