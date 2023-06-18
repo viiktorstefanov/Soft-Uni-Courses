3e340bb71422c92f1d50f9e75cda5d48f6e6e31a
viktor0110 <viktor-stefanov@mail.bg>
2023-06-18 20:09:32 +0300

    upload last tasks

--- SoftUniApplications/Remote Data and Authentication - Exercise/05.Fisher-Game/src/login.js
+++ SoftUniApplications/Remote Data and Authentication - Exercise/05.Fisher-Game/src/login.js
@@ -1,1 +1,45 @@
-console.log('TODO:// Implement Login functionality');
+(function login () {
+    let emailElement = document.querySelector('input[type="text"]');
+    let passwordElement = document.querySelector('input[type="password"]');
+    let form = document.querySelector('form');
+    document.getElementById('logout').style.display = 'none';
+    
+    let loginBtn = document.querySelector('#login button');
+    loginBtn.addEventListener('click', (e) => {
+        e.preventDefault();
+        
+        let email = emailElement.value;
+        let password = passwordElement.value;
+
+        let user = {
+            email,
+            password,
+        };
+
+        
+            fetch('http://localhost:3030/users/login', {
+                method: "POST",
+                headers: { 'Content-type': 'application/json'},
+                body: JSON.stringify(user),
+            })
+            .then(response => {
+                if(response.status !== 200) {
+                    throw new Error(response.message)
+                }
+                return response.json();
+
+            })
+            .then(data => {
+                
+                let token = data.accessToken;
+                localStorage.setItem('token', token);
+                localStorage.setItem('userData', JSON.stringify(data));
+                window.location.pathname = 'src/index.html';
+                
+            }).catch(error => {
+                    form.reset();
+                    alert(error.message)
+            }) 
+    })
+    
+})();
--- SoftUniApplications/Remote Data and Authentication - Exercise/05.Fisher-Game/src/register.js
+++ SoftUniApplications/Remote Data and Authentication - Exercise/05.Fisher-Game/src/register.js
@@ -1,1 +1,51 @@
-console.log('TODO:// Implement Register functionality');
+(function register() {
+document.getElementById('logout').style.display = 'none';
+let form = document.querySelector('#register-view #register');
+
+form.addEventListener('submit', (e) => {
+e.preventDefault();
+
+let formData = new FormData(e.target)
+let email = formData.get('email');
+let password = formData.get('password');
+let rePass = formData.get('rePass');
+
+if(email === "" || password === "" || rePass === "") {
+    throw new Error(`Invalid input`);
+}
+
+if(password !== rePass) {
+    throw new Error(`Passwords do NOT match`);
+}
+
+let newUser = {
+    email,
+    password,
+    rePass,
+};
+
+fetch('http://localhost:3030/users/register', {
+    method: 'POST',
+    headers: { 'Content-type': 'application/json'},
+    body: JSON.stringify(newUser),
+})
+.then(response => {
+    if(response.status !== 200) {
+        throw new Error(response.message)
+    }
+    return response.json();
+
+})
+.then(data => {
+    let token = data.accessToken;
+    localStorage.setItem('token', token);
+    localStorage.setItem('userData', JSON.stringify(data));
+    window.location.pathname = 'src/index.html';
+    
+}).catch(error => {
+        form.reset();
+        alert(error.message)
+}) 
+
+})
+})()
